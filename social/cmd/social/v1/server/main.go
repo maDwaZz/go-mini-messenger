8bab9444e0d3d90cac90441e9255cbe0b413ee4d
Василий Прокопенко <vprokopenko@sberbs.ru>
2025-09-22 01:21:57 +0300

    feat: add social service contracts

---
+++ social/cmd/social/v1/server/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	pb "go-mini-messenger/social/proto/api/social/v1"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type server struct {
+	pb.UnimplementedSocialServiceServer
+}
+
+func NewServer() *server {
+	return &server{}
+}
+
+func main() {
+	implementation := NewServer() // наша реализация сервера
+
+	lis, err := net.Listen("tcp", ":8083")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	server := grpc.NewServer()
+	pb.RegisterSocialServiceServer(server, implementation) // регистрация обработчиков
+
+	reflection.Register(server) // регистрируем дополнительные обработчики
+
+	log.Printf("server listening at %v", lis.Addr())
+	if err := server.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
